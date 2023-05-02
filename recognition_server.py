66652a4f33dae21a214e28da385a34883fc2cefb
Daniel <20088708@wit.ie>
2023-05-02 17:45:12 +0100

    Added requirements

--- recognition_server.py
+++ recognition_server.py
@@ -24,7 +24,7 @@ def run_recognition():
     known_face_encodings = []
     known_face_names = []
     for image in os.listdir('faces'):
-            face_image = face_recognition.load_image_file(f"faces/{image}")
+            face_image = face_recognition.load_image_file("faces/{image}")
             face_encoding = face_recognition.face_encodings(face_image)[0]
             known_face_encodings.append(face_encoding)
             known_face_names.append(image)
